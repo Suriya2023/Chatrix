1c82dba3e9bbd488f962f764367522e2247b4443
Suriya2023 <rajputsuraj221221@gmail.com>
2025-07-04 10:44:00 +0530

    minore changes

--- frontend/src/Database/axios.js
+++ frontend/src/Database/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5005/api',
+  baseURL: 'https://chatrix-49uu.onrender.com/api',
   withCredentials: true,
 });
